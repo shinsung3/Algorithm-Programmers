f96e4852712989532d41caff7f192ca224d9da72
shinsung <tlstjd436@naver.com>
2019-12-30 21:33:22 +0900

    Level1 - 문자열을 정수로 바꾸기

---
+++ Level1/Main_문자열을 정수로 바꾸기.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		String s = "01234";
+		int a = solution(s);
+		System.out.println(a);
+	}
+
+	public static int solution(String s) {
+		int answer = Integer.parseInt(s);
+		return answer;
+	}
+
+}
