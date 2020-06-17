8df56cf5423e23560d2a02262a81f864ac70be6c
shinsung <tlstjd436@naver.com>
2020-06-18 00:12:52 +0900

    Level3 - 등굣길

--- Level3/Main_등굣길.java
+++ Level3/Main_등굣길.java
@@ -11,27 +11,36 @@ public class Main_등굣길 {
 	}
 
 	static int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int[][] map = new int[n][m];
-        for(int i=0; i<puddles.length; i++) {
-        	map[puddles[i][1]-1][puddles[i][0]-1] = -1;
-        }
-        for(int i=0; i<m; i++) {
-        	map[0][i] = 1;
-        }
-        for(int i=0; i<n; i++) {
-        	map[i][0] = 1;
-        }
-        
-        for(int i=1; i<n; i++) {
-        	for(int j=1; j<m; j++) {
-        		if(map[i][j]==0)
-        			map[i][j] = (Math.max(map[i-1][j], map[i][j-1])+1)%1000000007;
-        	}
-        }
-        for(int[] a: map)
-        	System.out.println(Arrays.toString(a));
-        answer = map[n-1][m-1];
-        return answer;
-    }
+		int answer = 0;
+		int[][] map = new int[n][m];
+		boolean visit[][] = new boolean[n][m];
+		for (int i = 0; i < puddles.length; i++) {
+			visit[puddles[i][1] - 1][puddles[i][0] - 1] = true;
+		}
+		for (int i = 0; i < m; i++) {
+			if (!visit[0][i]) {
+				map[0][i] = 1;
+			}else {
+				break;
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (!visit[i][0]) {
+				map[i][0] = 1;
+			}else {
+				break;
+			}
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j < m; j++) {
+				if (!visit[i][j])
+					map[i][j] = (map[i - 1][j] + map[i][j - 1]) % 1000000007;
+			}
+		}
+		//for (int[] a : map)
+			//System.out.println(Arrays.toString(a));
+		answer = map[n - 1][m - 1];
+		return answer;
+	}
 }
