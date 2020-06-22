0d6b58fdb1144e74e3784308fcf297eddea45bbe
shinsung <tlstjd436@naver.com>
2020-06-23 00:33:32 +0900

    Level3 - 입국심사

---
+++ Level3/Main_입국심사.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class Main_입국심사 {
+	public static void main(String[] args) {
+		int n = 6;
+		int[] times = { 7, 10 };
+		long a = solution(n, times);
+		System.out.println(a);
+	}
+
+	static long solution(int n, int[] times) {
+		long answer = 0;
+		Arrays.sort(times);
+		//최적
+		long min = 1; 
+		//최악
+		long max = (long)times[times.length-1]*n;
+		long mid = 0;
+		long sum = 0;
+		answer = max;
+		while(min<=max) {
+			sum=0;
+			mid = (min+max)/2;
+			for(int t: times) {
+				sum += (long)(mid/t);
+			}
+			
+			if(sum>=n) {
+				if(mid<answer) {
+					answer = mid;
+				}
+				max = mid-1;
+			}else {
+				min = mid+1;
+			}
+		}
+		return answer;
+	}
+}
